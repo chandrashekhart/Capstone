678f64a96f8a6f85ad75c2d7338badb9a8b0579b
Chandrashekhar Tallapragada <50911171+chandrashekhart@users.noreply.github.com>
2023-10-19 00:12:33 +0530

    Update my_Python_script.py

--- .github/workflows/my_Python_script.py
+++ .github/workflows/my_Python_script.py
@@ -1,1 +1,23 @@
 
+import os
+import requests
+
+api_url = os.environ.get('API_URL')
+repository_url = os.popen('git config --get remote.origin.url').read().strip()
+
+payload = {
+    'repository_url': repository_url
+}
+
+headers = {
+    'Content-Type': 'application/json',
+    # Add any other headers as needed
+}
+
+response = requests.post(api_url, json=payload, headers=headers)
+
+if response.status_code == 200:
+    print("API request was successful.")
+else:
+    print(f"API request failed with status code {response.status_code}.")
+    print(response.text)
